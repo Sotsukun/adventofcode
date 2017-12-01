32af0e6ea1c475e2b7c4d2be070c0ddbd03c39c8
Sotsukun <Sotsukun@users.noreply.github.com>
2017-12-01 14:15:55 +0000

    Initial commit

---
+++ 1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+static uint8_t *str = 0;
+static uint16_t counter = 0;
+
+bool endOfString() {
+    return str[counter] == 0;
+}
+
+void nextChar()
+{
+    counter++;
+}
+
+uint8_t charToInt(uint8_t ch)
+{
+    return ch - '0';
+}
+
+uint8_t readInt() {
+    uint8_t result = 0;
+    if (str[counter] != 0) {
+        result = charToInt(str[counter]);
+    }
+    
+    return result;
+}
+
+uint8_t readNext() {
+    uint8_t result = 0;
+    if (str[counter+1] != 0) {
+        result = charToInt(str[counter+1]);
+    }
+    else {
+        result = charToInt(str[0]);
+    }
+    
+    return result;
+}
+
+uint8_t readHalfway() {
+    uint16_t len = strlen(str);
+    
+    return charToInt(str[(counter+len/2)%len]);
+}
+
+int main(int argc, char *argv[]) 
+{
+    uint16_t result1 = 0;
+    uint16_t result2 = 0;
+    
+    str = (uint8_t*)argv[1];
+    
+    while(!endOfString())
+    {
+        if(readInt() == readNext()) {
+            result1 += readInt();
+        }
+        
+        if(readInt() == readHalfway()) {
+            result2 += readInt();
+        }
+        
+        nextChar();
+    }
+    
+    printf("%d\r\n", result1);
+    printf("%d\r\n", result2);
+}
