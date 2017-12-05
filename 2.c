e8a56b24a472ab53ac1fff99eb98bdb9a54c4688
Sotsukun <Sotsukun@users.noreply.github.com>
2017-12-05 10:14:43 +0000

    Completed day 2, part 1

--- 2.c
+++ 2.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "common.h"
 
@@ -10,7 +11,7 @@
 typedef enum {
     SUCCESS,
     EOL,
-    EOF,
+    EOFILE,
     ERROR
 } readLineResult_t;
 
@@ -28,7 +29,6 @@ void setString(uint8_t *pStr)
 readLineResult_t readInt(uint16_t *pNum)
 {
     uint8_t *pReadChar = pString;
-    uint8_t numLength = 0;
 
     if(pReadChar == NULL) {
         return ERROR;
@@ -38,7 +38,7 @@ readLineResult_t readInt(uint16_t *pNum)
         return EOL;
     }
     if(*pReadChar == '\0') {
-        return EOF;
+        return EOFILE;
     }
 
     // Read ahead to find if an endline is reached before the next number
@@ -48,16 +48,16 @@ readLineResult_t readInt(uint16_t *pNum)
             break;
         }
 
-        numLength++;
         pReadChar++;
     }
 
     // Skip the next space
     while(*pReadChar == ' ')
     {
-        numLength++;
         pReadChar++;
     }
+    
+    uint8_t *pNextChar = pReadChar;
 
     // Reset the string pointer
     pReadChar = pString;
@@ -66,20 +66,76 @@ readLineResult_t readInt(uint16_t *pNum)
     *pNum = atoi((const char*)pReadChar);
 
     // Move the string pointer to the next number
-    pString += numLength;
+    pString = pNextChar;
 
     return SUCCESS;
+}
+
+bool isEndLine()
+{
+    return *pString == '\n';
+}
+
+void moveToNextLine()
+{
+    while(*pString != '\n') {
+        pString++;
+    }
+    pString++;
+}
+
+bool isEndOfFile()
+{
+    return *pString == '\0';
+}
+
+uint16_t getLineDiff()
+{
+    uint16_t minVal = UINT16_MAX;
+    uint16_t maxVal = 0;
+    
+    while(!isEndLine()) {
+        uint16_t num = 0;
+        
+        readInt(&num);
+        
+        minVal = minVal < num ? minVal : num;
+        maxVal = maxVal > num ? maxVal : num;
+    }
+    
+    moveToNextLine();
+    
+    return maxVal - minVal;
 }
 
 
 int main(int argc, char *argv[])
 {
-    uint8_t *numbers =  "1234 2345 235 34556342\n"
+    uint8_t *numbers =  "5048    177     5280    5058    4504    3805    5735    220     4362    1809    1521    230     772     1088    178     1794\n"
+                        "6629    3839    258     4473    5961    6539    6870    4140    4638    387     7464    229     4173    5706    185     271\n"
+                        "5149    2892    5854    2000    256     3995    5250    249     3916    184     2497    210     4601    3955    1110    5340\n"
+                        "153     468     550     126     495     142     385     144     165     188     609     182     439     545     608     319\n"
+                        "1123    104     567     1098    286     665     1261    107     227     942     1222    128     1001    122     69      139\n"
+                        "111     1998    1148    91      1355    90      202     1522    1496    1362    1728    109     2287    918     2217    1138\n"
+                        "426     372     489     226     344     431     67      124     120     386     348     153     242     133     112     369\n"
+                        "1574    265     144     2490    163     749     3409    3086    154     151     133     990     1002    3168    588     2998\n"
+                        "173     192     2269    760     1630    215     966     2692    3855    3550    468     4098    3071    162     329     3648\n"
+                        "1984    300     163     5616    4862    586     4884    239     1839    169     5514    4226    5551    3700    216     5912\n"
+                        "1749    2062    194     1045    2685    156     3257    1319    3199    2775    211     213     1221    198     2864    2982\n"
+                        "273     977     89      198     85      1025    1157    1125    69      94      919     103     1299    998     809     478\n"
+                        "1965    6989    230     2025    6290    2901    192     215     4782    6041    6672    7070    7104    207     7451    5071\n"
+                        "1261    77      1417    1053    2072    641     74      86      91      1878    1944    2292    1446    689     2315    1379\n"
+                        "296     306     1953    3538    248     1579    4326    2178    5021    2529    794     5391    4712    3734    261     4362\n"
+                        "2426    192     1764    288     4431    2396    2336    854     2157    216     4392    3972    229     244     4289    1902\n";
     uint16_t num = 0;
 
     setString(numbers);
-
-    while(readInt(&num) == SUCCESS) {
-        printf("%d\r\n", num);
+    
+    while(!isEndOfFile())
+    {
+        uint16_t diff = getLineDiff();
+        num += diff;
     }
+    
+    printf("Checksum: %d\r\n", num);
 }
